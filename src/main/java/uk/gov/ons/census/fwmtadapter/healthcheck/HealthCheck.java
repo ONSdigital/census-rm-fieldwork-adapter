a42967d331bfb780601ac14a13c6fb4cdc4e5432
Nick Grant <nick.grant@ext.ons.gov.uk>
2019-07-19 09:23:15 +0100

    Rename misspelled class

--- src/main/java/uk/gov/ons/census/fwmtadapter/healthcheck/HealthCheck.java
+++ src/main/java/uk/gov/ons/census/fwmtadapter/healthcheck/HealthCheck.java
@@ -11,7 +11,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-public class HeathCheck {
+public class HealthCheck {
   @Value("${healthcheck.filename}")
   private String fileName;
 
