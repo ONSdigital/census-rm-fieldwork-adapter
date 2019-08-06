7bfea04cb88344c8c6669e2fbe7b590c63dfe339
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2019-08-06 08:44:35 +0100

    tests passing

--- src/test/java/uk/gov/ons/census/fwmtadapter/messaging/RefusalReceiverIT.java
+++ src/test/java/uk/gov/ons/census/fwmtadapter/messaging/RefusalReceiverIT.java
@@ -7,7 +7,6 @@ import java.util.concurrent.BlockingQueue;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
-import org.jeasy.random.EasyRandom;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,7 +18,11 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+import uk.gov.ons.census.fwmtadapter.model.dto.CollectionCase;
+import uk.gov.ons.census.fwmtadapter.model.dto.Event;
 import uk.gov.ons.census.fwmtadapter.model.dto.EventType;
+import uk.gov.ons.census.fwmtadapter.model.dto.Payload;
+import uk.gov.ons.census.fwmtadapter.model.dto.Refusal;
 import uk.gov.ons.census.fwmtadapter.model.dto.ResponseManagementEvent;
 import uk.gov.ons.census.fwmtadapter.model.dto.field.ActionInstruction;
 import uk.gov.ons.census.fwmtadapter.util.RabbitQueueHelper;
@@ -55,17 +58,26 @@ public class RefusalReceiverIT {
   public void testReceiveMessage() throws InterruptedException, JAXBException {
     BlockingQueue<String> outboundQueue = rabbitQueueHelper.listen(actionOutboundQueue);
 
-    EasyRandom easyRandom = new EasyRandom();
-    ResponseManagementEvent event = easyRandom.nextObject(ResponseManagementEvent.class);
-    event.getEvent().setType(EventType.REFUSAL_RECEIVED);
-    rabbitQueueHelper.sendMessage(caseEventExchange, refusalRoutingKey, event);
+    CollectionCase collectionCase = new CollectionCase();
+    collectionCase.setId("123");
+    Refusal refusal = new Refusal();
+    refusal.setCollectionCase(collectionCase);
+    Payload payload = new Payload();
+    payload.setRefusal(refusal);
+    ResponseManagementEvent responseManagementEvent = new ResponseManagementEvent();
+    responseManagementEvent.setPayload(payload);
+    Event event = new Event();
+    event.setType(EventType.REFUSAL_RECEIVED);
+    responseManagementEvent.setEvent(event);
+
+    rabbitQueueHelper.sendMessage(caseEventExchange, refusalRoutingKey, responseManagementEvent);
 
     String actualMessage = rabbitQueueHelper.getMessage(outboundQueue);
     JAXBContext jaxbContext = JAXBContext.newInstance(ActionInstruction.class);
     Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
     StringReader reader = new StringReader(actualMessage);
     ActionInstruction actionInstruction = (ActionInstruction) unmarshaller.unmarshal(reader);
-    assertThat(event.getPayload().getRefusal().getCollectionCase().getId())
+    assertThat(responseManagementEvent.getPayload().getRefusal().getCollectionCase().getId())
         .isEqualTo(actionInstruction.getActionCancel().getCaseId());
     assertThat("REFUSED").isEqualTo(actionInstruction.getActionCancel().getReason());
   }
