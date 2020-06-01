a15ed83ec1101d8e584e8d628ac088a82e3e8159
Nick Grant <nick.grant@ext.ons.gov.uk>
2020-06-01 09:09:21 +0100

    Update MessageConsumerConfig.java

--- src/main/java/uk/gov/ons/census/fwmtadapter/config/MessageConsumerConfig.java
+++ src/main/java/uk/gov/ons/census/fwmtadapter/config/MessageConsumerConfig.java
@@ -1,13 +1,18 @@
 package uk.gov.ons.census.fwmtadapter.config;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.AbstractMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.amqp.inbound.AmqpInboundChannelAdapter;
+import org.springframework.integration.amqp.support.DefaultAmqpHeaderMapper;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.retry.backoff.FixedBackOffPolicy;
@@ -60,19 +65,14 @@ public class MessageConsumerConfig {
   public AmqpInboundChannelAdapter actionFieldInbound(
       @Qualifier("actionFieldContainer") SimpleMessageListenerContainer listenerContainer,
       @Qualifier("actionFieldInputChannel") MessageChannel channel) {
-    AmqpInboundChannelAdapter adapter = new AmqpInboundChannelAdapter(listenerContainer);
-    adapter.setOutputChannel(channel);
-
-    return adapter;
+    return makeAdapter(listenerContainer, channel);
   }
 
   @Bean
   public AmqpInboundChannelAdapter caseReceivedInbound(
       @Qualifier("caseReceivedContainer") SimpleMessageListenerContainer listenerContainer,
       @Qualifier("caseReceiverChannel") MessageChannel channel) {
-    AmqpInboundChannelAdapter adapter = new AmqpInboundChannelAdapter(listenerContainer);
-    adapter.setOutputChannel(channel);
-    return adapter;
+    return makeAdapter(listenerContainer, channel);
   }
 
   @Bean
@@ -113,5 +113,27 @@ public class MessageConsumerConfig {
 
     container.setAdviceChain(retryOperationsInterceptor);
     return container;
+  }
+
+  private AmqpInboundChannelAdapter makeAdapter(
+      AbstractMessageListenerContainer listenerContainer, MessageChannel channel) {
+    AmqpInboundChannelAdapter adapter = new AmqpInboundChannelAdapter(listenerContainer);
+    adapter.setOutputChannel(channel);
+    adapter.setHeaderMapper(
+        new DefaultAmqpHeaderMapper(null, null) {
+          @Override
+          public Map<String, Object> toHeadersFromRequest(MessageProperties source) {
+            Map<String, Object> headers = new HashMap<>();
+
+            /* We strip EVERYTHING out of the headers and put the content type back in because we
+             * don't want the __TYPE__ to be processed by Spring Boot, which would cause
+             * ClassNotFoundException because the type which was sent doesn't match the type we
+             * want to receive. This is an ugly workaround, but it works.
+             */
+            headers.put("contentType", "application/json");
+            return headers;
+          }
+        });
+    return adapter;
   }
 }
